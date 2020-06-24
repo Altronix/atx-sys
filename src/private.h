3cd6b2d63ebbdb1ee4506646721adf2253f225f6
thomas <thomas.chiantia@gmail.com>
2020-06-24 15:12:31 -0400

    file adds

---
+++ src/private.h
@@ -0,0 +1,13 @@
+#ifndef PRIVATE_H
+#define PRIVATE_H
+
+#include <assert.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#endif /* PRIVATE_H */
