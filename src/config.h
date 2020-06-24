acba62d1f93bffd1cb3f16eac731fecde3eafe90
thomas <thomas.chiantia@gmail.com>
2020-06-24 16:00:02 -0400

    file addss

---
+++ src/config.h
@@ -0,0 +1,8 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#ifndef ATX_UPDATE_ENV_CONFIG_FILE
+#define ATX_UPDATE_ENV_CONFIG_FILE "/env/fw_env.config"
+#endif
+
+#endif /* CONFIG_H */
---
+++ src/sys.h
@@ -0,0 +1,8 @@
+#ifndef SYS_H
+#define SYS_H
+
+#include "private.h"
+
+int32_t tick();
+
+#endif /* SYS_H */
