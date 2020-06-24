8b1d3a9150c0595a46383f84afee4100db14bc95
thomas <thomas.chiantia@gmail.com>
2020-06-24 14:57:09 -0400

    cmake format

--- cmake/libubootenv.cmake
+++ cmake/libubootenv.cmake
@@ -1,45 +1,54 @@
 set(LIBUBOOTENV_SOURCE_DIR ${CMAKE_SOURCE_DIR}/external/libubootenv)
 
 if(NOT EXISTS ${LIBUBOOTENV_SOURCE_DIR}/CMakeLists.txt)
-  execute_process(
-    COMMAND ${GIT_EXECUTABLE} submodule update --init external/libubootenv
-    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-    RESULT_VARIABLE GIT_CLONE_LIBUBOOTENV_RESULT)
-  message(STATUS "GIT_CLONE_LIBUBOOTENV_RESULT: ${GIT_CLONE_LIBUBOOTENV_RESULT}")
+  execute_process(COMMAND ${GIT_EXECUTABLE}
+                          submodule
+                          update
+                          --init
+                          external/libubootenv
+                  WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+                  RESULT_VARIABLE GIT_CLONE_LIBUBOOTENV_RESULT)
+  message(
+    STATUS "GIT_CLONE_LIBUBOOTENV_RESULT: ${GIT_CLONE_LIBUBOOTENV_RESULT}")
 endif()
 
 ExternalProject_Add(libubootenv-project
-	SOURCE_DIR ${LIBUBOOTENV_SOURCE_DIR}
-	INSTALL_DIR ${CMAKE_INSTALL_PREFIX}
-	UPDATE_COMMAND ""
-	BUILD_COMMAND ""
-	INSTALL_COMMAND
-		cmake
-		--build .
-		--target install
-		--config MinSizeRel
-	LIST_SEPARATOR |
-	CMAKE_ARGS 
-		-DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
-		-DBUILD_DOC=FALSE
-	)
-
+                    SOURCE_DIR ${LIBUBOOTENV_SOURCE_DIR}
+                    INSTALL_DIR ${CMAKE_INSTALL_PREFIX}
+                    UPDATE_COMMAND ""
+                    BUILD_COMMAND ""
+                    INSTALL_COMMAND cmake
+                                    --build
+                                    .
+                                    --target
+                                    install
+                                    --config
+                                    MinSizeRel
+                    LIST_SEPARATOR |
+                    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
+                               -DBUILD_DOC=FALSE)
 
 ExternalProject_Get_Property(libubootenv-project install_dir)
 set(libubootenv_INCLUDE_DIR ${install_dir}/include)
-FILE(MAKE_DIRECTORY ${install_dir}/include)
-set(libubootenv_static_LIBRARY ${install_dir}/lib/liblibubootenv${CMAKE_STATIC_LIBRARY_SUFFIX})
-set(libubootenv_shared_LIBRARY ${install_dir}/bin/liblibubootenv${CMAKE_SHARED_LIBRARY_SUFFIX})
+file(MAKE_DIRECTORY ${install_dir}/include)
+set(libubootenv_static_LIBRARY
+    ${install_dir}/lib/liblibubootenv${CMAKE_STATIC_LIBRARY_SUFFIX})
+set(libubootenv_shared_LIBRARY
+    ${install_dir}/bin/liblibubootenv${CMAKE_SHARED_LIBRARY_SUFFIX})
 message(STATUS "libubootenv_shared_LIBRARY: ${libubootenv_shared_LIBRARY}")
 
 # libubootenv-static
 add_library(libubootenv-static STATIC IMPORTED)
 add_dependencies(libubootenv-static libubootenv-project)
-set_property(TARGET libubootenv-static PROPERTY IMPORTED_LOCATION ${libubootenv_static_LIBRARY})
-set_property(TARGET libubootenv-static PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${libubootenv_INCLUDE_DIR})
+set_property(TARGET libubootenv-static
+             PROPERTY IMPORTED_LOCATION ${libubootenv_static_LIBRARY})
+set_property(TARGET libubootenv-static
+             PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${libubootenv_INCLUDE_DIR})
 
 # libubootenv-shared
 add_library(libubootenv-shared SHARED IMPORTED)
 add_dependencies(libubootenv-shared libubootenv-project)
-set_property(TARGET libubootenv-shared PROPERTY IMPORTED_LOCATION ${libubootenv_shared_LIBRARY})
-set_property(TARGET libubootenv-shared PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${libubootenv_INCLUDE_DIR})
+set_property(TARGET libubootenv-shared
+             PROPERTY IMPORTED_LOCATION ${libubootenv_shared_LIBRARY})
+set_property(TARGET libubootenv-shared
+             PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${libubootenv_INCLUDE_DIR})
