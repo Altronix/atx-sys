825bd75e94816e7ce7ac8f5375bd231f7c3f93b1
thomas <thomas.chiantia@gmail.com>
2020-06-25 13:14:22 -0400

    fixed

--- cmake/mongoose.cmake
+++ cmake/mongoose.cmake
@@ -1,18 +1,13 @@
 set(MONGOOSE_SOURCE_DIR ${CMAKE_SOURCE_DIR}/external/mongoose)
 set(MONGOOSE_TEST_FILE ${MONGOOSE_SOURCE_DIR}/CMakeLists.txt)
 
-ExternalProject_Add(mongoose-project
-  SOURCE_DIR ${MONGOOSE_SOURCE_DIR}
-  INSTALL_DIR ${CMAKE_INSTALL_PREFIX}
-  CONFIGURE_COMMAND ""
-  BUILD_COMMAND COMMAND 
-		  ${CMAKE_C_COMPILER} ${CMAKE_C_FLAGS} -fpic -c ${MONGOOSE_SOURCE_DIR}/mongoose.c 
-		  -DMG_ENABLE_HTTP_STREAMING_MULTIPART
-		  -DMG_ENABLE_WEBSOCKET
-		  -DMG_ENABLE_CALLBACK_USERDATA
-		COMMAND 
-		  ${CMAKE_COMMAND} -E copy ${MONGOOSE_SOURCE_DIR}/mongoose.h ${CMAKE_INSTALL_PREFIX}/include/mongoose.h
-  INSTALL_COMMAND ${CMAKE_C_COMPILER} -shared -o ${CMAKE_INSTALL_PREFIX}/lib/libmongoose.so mongoose.o)
+add_library(mongoose-shared SHARED 
+  ${MONGOOSE_SOURCE_DIR}/mongoose.c 
+  ${MONGOOSE_SOURCE_DIR}/mongoose.h)
+install(TARGETS mongoose-shared
+        LIBRARY DESTINATION lib
+        ARCHIVE DESTINATION lib
+        PUBLIC_HEADER DESTINATION include)
 
 function(append_mongoose_compiler_flags dst)
   list(APPEND definitions "-DMG_ENABLE_HTTP_STREAMING_MULTIPART")
