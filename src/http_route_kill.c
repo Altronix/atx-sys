f3f439ff1d5fd3978f4089ef03de8cbeace0d93e
thomas <thomas.chiantia@gmail.com>
2020-07-13 15:32:51 -0400

    add response before shutting down

--- src/http_route_kill.c
+++ src/http_route_kill.c
@@ -3,6 +3,13 @@
 void
 route_kill(route_context* ctx, HTTP_METHOD meth, uint32_t l, const char* b)
 {
+    char resp[256];
+    snprintf(
+        resp,
+        sizeof(resp),
+        "{\"error\":200,\"message\":\"%s\"}",
+        http_error_message(200));
     http_s* http = ctx->context;
     http->shutdown = true;
+    http_printf_json(ctx->connection, 200, resp);
 }
